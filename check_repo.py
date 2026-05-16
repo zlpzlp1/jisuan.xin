d4139c4642e62605e3c233addfa27b8cf77502dd
zlpzlp1 <zlpzlp1@users.noreply.github.com>
2026-05-16 15:38:19 +0800

    clean: 移除工具文件

--- check_repo.py
+++
@@ -1,7 +0,0 @@
-import urllib.request, json
-req = urllib.request.Request('https://api.github.com/repos/zlpzlp1/jisuan.xin/contents/', headers={'User-Agent': 'jisuan'})
-r = urllib.request.urlopen(req, timeout=10)
-data = json.loads(r.read())
-for item in data:
-    print(item['name'], item['type'])
-print('OK', len(data))
